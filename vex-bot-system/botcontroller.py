6a557a114b3b632d0d3458bc6b6536eb4ede0041
GoblinSharks <101782320+Gob1inSharks@users.noreply.github.com>
2024-09-29 02:30:34 -0400

    Create botcontroller.py
    todo: mix update motors

---
+++ vex-bot-system/botcontroller.py
@@ -0,0 +1,30 @@
+
+class Controller:
+
+    def __init__(self,vexbot):
+
+        self.bot = vexbot
+
+        #V = self.Kp * (theta(t)-theta(t-1)) + self.Kd * (theta(t))
+        self.Kp = 0.1
+        self.Kd = 0.1
+        #self.Ki = #the integral can be neglected here
+
+        self.target = 0 #the target angle you want the bot to reach
+
+        #self.theta = 0
+
+        self.previous_step = self.bot.gyro
+        self.current_step = self.bot.gyro #the current difference in the voltages of the bot
+        self.advanced_step = self.target
+
+    def updateMotors(self):
+
+        self.current_step = self.bot.gyro
+        V = self.Kp * (self.current_step-self.previous_step) + self.Kd * self.current_step
+        self.previous_step = self.current_step
+
+        if V > 0:
+            self.bot.motor[0] -= V
+        elif V < 0:
+            self.bot.motor[1] -= V
